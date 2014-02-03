20b94e95f91d95e666bd0870149133c36f3058ac
Matt Bertrand <mbertrand@cga.harvard.edu>
2014-02-02 19:04:31 -0500

    Add build files

---
+++ setup.py
@@ -0,0 +1,69 @@
+import os
+from setuptools import setup, find_packages
+from distutils.command.install import INSTALL_SCHEMES
+import os
+import sys
+
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Tell distutils not to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+geonode_dir = 'worldmap'
+
+for dirpath, dirnames, filenames in os.walk(geonode_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="worldmap",
+    version="2.0",
+    author="GeoNode and WorldMap developers",
+    author_email='worldmap@harvard.edu',
+    license='GPL',
+    description="WorldMap, based on GeoNode",
+    long_description=(read('README.rst')),
+    # Full list of classifiers can be found at:
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+    keywords="worldmap geonode django",
+    url='https://github.com/cga-harvard/cga-worldmap',
+    packages=packages,
+    data_files=data_files,
+    include_package_data=True,
+    zip_safe=False,
+)
