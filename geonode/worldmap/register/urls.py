1927fb00e67dc30e56abb3d329153d384de555bb
Matt Bertrand <mbertrand@cga.harvard.edu>
2014-05-23 19:34:28 -0400

    Fix url import

--- geonode/worldmap/register/urls.py
+++ geonode/worldmap/register/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 import account.views
 from geonode.worldmap.register.views import SignupView, registercompleteOrganizationUser, forgot_username
 
