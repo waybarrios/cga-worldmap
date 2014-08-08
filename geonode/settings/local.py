40e8a9cce77338c6ba9fc7cd35ef4f735bb3af01
Matt Bertrand <mbertrand@cga.harvard.edu>
2014-08-08 13:45:17 -0400

    Clear out settings

--- geonode/settings/local.py
+++ geonode/settings/local.py
@@ -2,15 +2,3 @@ from .base import *
 
 #Enter local settings here
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'wm_db_django15',
-        'USER': 'wm_user',
-        'PASSWORD': 'wm_password',
-        'HOST': 'localhost', 'PORT': '5432'
-        }
-}
-
-GEONODE_CLIENT_LOCATION = "http://localhost:9090/"
-HAYSTACK_SEARCH = False
