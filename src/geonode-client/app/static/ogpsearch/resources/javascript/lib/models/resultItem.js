67c801cfacabf9e7627aa4092fabb5de0b92c8d7
Steve McDonald <SpacemanSteve@gmail.com>
2015-03-04 08:56:35 -0500

    preliminary heatmap code using Solr array of ints, not png a couple global functions that create a vector layer with shaded rectangles

--- src/geonode-client/app/static/ogpsearch/resources/javascript/lib/models/resultItem.js
+++ src/geonode-client/app/static/ogpsearch/resources/javascript/lib/models/resultItem.js
@@ -53,19 +53,21 @@ OpenGeoportal.ResultsCollection = Backbone.Collection.extend({
 				}
 			});
 			// temp code to pull out heatmap png from solr response and display
-			if (typeof heatmapLayer != 'undefined')
-			    {
-				console.log("removing layer");
-				OpenGeoportal.ogp.map.removeLayer(heatmapLayer);
-			    }
+			removeHeatmap();
 			tempResponse = dataObj;
-			facetCounts = tempResponse.facet_counts;
-			facetHeatmaps = facetCounts.facet_heatmaps;
-			bbox_rpt = facetHeatmaps.bbox_rpt;
-			heatmapPng = bbox_rpt[15];
+			var facetCounts = tempResponse.facet_counts;
+			var facetHeatmaps = facetCounts.facet_heatmaps;
+			var bbox_rpt = facetHeatmaps.bbox_rpt;
+
+			var heatmap = bbox_rpt[15];
+			if (heatmap != null)
+			    drawHeatmap(bbox_rpt);
+
 			// create Image object to get png width and height
 			// layer creation based on
 			//   http://gis.stackexchange.com/questions/58607/displaying-a-multi-zoom-image-layer-in-open
+			/* old code to display Solr heatmap png
+			heatmapPng = bbox_rpt[15];
 			img = new Image();
 			img.src = "data:image/png;base64," + heatmapPng;
 			bounds = OpenGeoportal.ogp.map.getExtent().transform(OpenGeoportal.ogp.map.projection, new OpenLayers.Projection("EPSG:4326"));
@@ -73,11 +75,11 @@ OpenGeoportal.ResultsCollection = Backbone.Collection.extend({
 			heatmapLayer = new OpenLayers.Layer.Image( 'Heatmap', "data:image/png;base64," + heatmapPng,
 								  new OpenLayers.Bounds(bounds2.left,bounds2.bottom,bounds2.right,bounds2.top), 
 								  new OpenLayers.Size(img.width, img.height),
-{opacity: 0.7});
+                                  {opacity: 0.7});
 
 			OpenGeoportal.ogp.map.addLayer(heatmapLayer);
 			console.log("added heapmap layer to map");
-
+            */
 			// solr docs holds an array of hashtables, each hashtable contains a
 			// layer
 			var arrModels = [];
@@ -182,3 +184,135 @@ OpenGeoportal.ResultsCollection = Backbone.Collection.extend({
 		}
 
 });
+
+function drawBox()
+{
+
+    boxes  = new OpenLayers.Layer.Boxes("Boxes")
+    //coordinates = [boundingbox.left, boundingbox.bottom,boundingbox.right, boundingbox.top]
+    coordinates = [10, 10, 20, 20];
+
+    bounds = OpenLayers.Bounds.fromArray(coordinates).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"))
+    box = new OpenLayers.Marker.Box(bounds, null, style)
+    ///box.setBorder("blue")
+    box.setStyle(style);
+    boxes.addMarker(box)
+    OpenGeoportal.ogp.map.addLayers([boxes])
+}
+
+function drawBox2()
+{
+var style = {
+   strokeColor: "#00FF00",
+   strokeOpacity: 1,
+   strokeWidth: 0,
+   fillColor: "#FF0000",
+   fillOpacity: 0.3
+    };
+    layer = new OpenLayers.Layer.Vector("boxes");
+    coordinates = [10, 10, 20, 20];
+    bounds = OpenLayers.Bounds.fromArray(coordinates).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"));
+    box = new OpenLayers.Feature.Vector(bounds.toGeometry(), null, style);
+    layer.addFeatures(box);
+
+style = {
+   strokeColor: "#00FF00",
+   strokeOpacity: 1,
+   strokeWidth: 0,
+   fillColor: "#00FF00",
+   fillOpacity: 0.6
+    };
+    coordinates = [20, 20, 30, 30];
+    bounds = OpenLayers.Bounds.fromArray(coordinates).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"));
+    style.fillColor = "#00FF00";
+    box = new OpenLayers.Feature.Vector(bounds.toGeometry(), null, style);
+    layer.addFeatures(box);
+
+    OpenGeoportal.ogp.map.addLayers([layer]);
+
+}
+
+function removeHeatmap()
+{
+    if (heatmapLayer != null)
+	{
+	    console.log("removing layer");
+		OpenGeoportal.ogp.map.removeLayer(heatmapLayer);
+		heatmapLayer = null;
+	}
+}
+
+// this variable should be moved
+// perhaps as a member variable in a heatmap object
+heatmapLayer = null;
+
+/**
+  return the largest value in the heatmap so its values can be scaled
+  some elements in the heatmap can be null so we replace them with 0
+*/
+function heatmapMax(heatmap)
+{
+   var max = -1;
+   for (i = 0 ; i < heatmap.length ; i++)
+   {
+       var currentRow = heatmap[i];
+       if (currentRow == null)  heatmap[i] = currentRow = [];
+       for (j = 0 ; j < currentRow.length ; j++)
+       {
+           if (currentRow[j] == null) currentRow[j] = 0;
+           if (currentRow[j] > max)
+              max = currentRow[j];
+       }
+   }
+   return max;
+}
+
+/**
+  the passed heatmap object is an array that contains the heatmap 2d array
+   as well as its geodetic bounds
+  the heatmap is displayed by creating a grid of translucent red rectangles
+*/
+function drawHeatmap(heatmapObject)
+{
+    heatmap = heatmapObject[15];
+    var maxValue = heatmapMax(heatmap);
+    if (maxValue == -1) return;  // something wrong
+
+    var minimumLatitude = heatmapObject[11];
+    var maximumLatitude = heatmapObject[13];
+    var deltaLatitude = maximumLatitude - minimumLatitude;
+    var minimumLongitude = heatmapObject[7];
+    var maximumLongitude = heatmapObject[9];
+    var deltaLongitude = maximumLongitude - minimumLongitude;
+
+    var stepsLatitude = heatmap.length;
+    var stepsLongitude = heatmap[0].length;
+    var stepSizeLatitude = deltaLatitude / stepsLatitude;
+    var stepSizeLongitude = deltaLongitude / stepsLongitude;
+    heatmapLayer = new OpenLayers.Layer.Vector("heatmap");
+
+    for (i = 0 ; i < stepsLatitude ; i++)
+    {
+        for (j = 0 ; j < stepsLongitude ; j++)
+        {
+            var heatmapValue = heatmap[heatmap.length - i - 1][j];
+            var scaledHeatmapValue = Math.floor((heatmapValue / maxValue) * 255);
+            var rgb = scaledHeatmapValue << 16;
+            var color = '#' + rgb.toString(16);
+            var coordinates = [minimumLongitude + (j * stepSizeLongitude), minimumLatitude + (i * stepSizeLatitude),
+                           minimumLongitude + ((j+1) * stepSizeLongitude), minimumLatitude + ((i+1) * stepSizeLatitude)];
+            var bounds = OpenLayers.Bounds.fromArray(coordinates).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"));
+            // we should create 255 separate styles and use the right one rather than making a new style for each tile
+            var style = {strokeWidth: 0, fillOpacity: 0.4};
+            style.fillColor = color;
+            var box = new OpenLayers.Feature.Vector(bounds.toGeometry(), null, style);
+            heatmapLayer.addFeatures(box);
+            if ((i == 0) && (j == 0))
+                console.log(coordinates);
+            if ((i == (stepsLatitude-1)) && (j == (stepsLongitude-1)))
+                console.log(coordinates);
+        }
+    }
+    OpenGeoportal.ogp.map.addLayers([heatmapLayer]);
+
+}
