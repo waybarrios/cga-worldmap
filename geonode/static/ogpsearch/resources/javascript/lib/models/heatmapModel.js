820ba26dae5c0c983d7db0a885822adf4d7ee038
Steve McDonald <SpacemanSteve@gmail.com>
2015-08-06 11:20:18 -0400

    changed heatmap colors

--- geonode/static/ogpsearch/resources/javascript/lib/models/heatmapModel.js
+++ geonode/static/ogpsearch/resources/javascript/lib/models/heatmapModel.js
@@ -19,7 +19,7 @@ OpenGeoportal.Models.Heatmap = Backbone.Model.extend(
 	    // add listener for seach events
 	    that.backgroundLayer = new OpenLayers.Layer.Stamen("toner-lite");
 	    OpenGeoportal.ogp.map.addLayer(that.backgroundLayer);
-	    OpenGeoportal.Models.Heatmap.radiusAdjust = 1.;  // only used for testing
+	    OpenGeoportal.Models.Heatmap.radiusAdjust = 1.1;  // scales all heatmap circles
             jQuery(document).on("fireSearch", function()
                 {
                     that.handleHeatmap(that);
@@ -178,7 +178,7 @@ OpenGeoportal.Models.Heatmap = Backbone.Model.extend(
 	    flattenedValues = that.flattenValues(heatmap);
             series = new geostats(flattenedValues);
 
-            jenksClassifications = series.getClassJenks(5);
+            jenksClassifications = series.getClassJenks(6);
 	    for (var i = 0 ; i < jenksClassifications.length ; i++)
 	    {
 		if (jenksClassifications[i] < 0)
@@ -192,7 +192,8 @@ OpenGeoportal.Models.Heatmap = Backbone.Model.extend(
 	*/
 	getColorGradient: function(that, classifications)
 	{
-            colors = [0x00000000, 0xfef0d9ff, 0xfdcc8aff, 0xfc8d59ff, 0xe34a33ff, 0xb30000ff];
+            //colors = [0x00000000, 0xfef0d9ff, 0xfdcc8aff, 0xfc8d59ff, 0xe34a33ff, 0xb30000ff];
+	    colors = [0x00000000, 0x0000dfff, 0x00effeff, 0x00ff42ff, 0xfeec30ff, 0xff5f00ff, 0xff0000ff];
             colorGradient = {};
             for (var i = 0 ; i < classifications.length ; i++)
 	    {
