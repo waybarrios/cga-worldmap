5f86b97a65913c2f8156b6349736abf4c1f4420a
Jude Mwenda <judemwenda@gmail.com>
2015-09-28 12:42:35 -0400

    Revert "Changes to allow for urls such as http://giswebservices.massgis.state…"

--- geonode/ogpsearch/utils.py
+++ geonode/ogpsearch/utils.py
@@ -15,7 +15,6 @@ from arcrest import Folder as ArcFolder, MapService as ArcMapService
 import logging
 import time
 from geonode.layers.models import Layer
-from geonode.services.models import Service
 from urlparse import urlparse
 
 #from geonode.services.models import Service, Layer, ServiceLayer, WebServiceHarvestLayersJob, WebServiceRegistrationJob
@@ -50,7 +49,10 @@ class OGP_utils(object):
         hostname = urlParts.hostname
         if hostname == "localhost":
             return "Harvard" # assumption
-        return hostname
+        domainParts = hostname.split(".")
+        if len(domainParts) == 1:
+            return domainParts[0]
+        return domainParts[-2].capitalize()
         
 
     @staticmethod
@@ -90,11 +92,9 @@ class OGP_utils(object):
                 if (layer.is_vector()):
                     dataType = "Polygon"
                 institution = "Harvard"
-                servicetype = None;
-                owsUrl = layer.ows_url
                 if layer.storeType == "remoteStore":
                     institution = "Remote"
-                    servicetype = Service.objects.get(base_url=owsUrl).type
+                owsUrl = layer.ows_url
                 domain = OGP_utils.get_domain(owsUrl)
                 if (i == 0):
                     i = layer.title
@@ -102,10 +102,9 @@ class OGP_utils(object):
                 OGP_utils.solr.add([{"LayerId": "HarvardWorldMapLayer_" + str(i), 
                                  "Name": layer.title,  
                                  "LayerDisplayName": layer.title,
-                                 "Institution": institution,
+                                 "Institution": "WorldMap",
                                  "Publisher": username,
                                  "Originator": domain,
-                                 "ServiceType": servicetype,
                                  "Access": "Public",
                                  "DataType": dataType, 
                                  "Availability": "Online",
